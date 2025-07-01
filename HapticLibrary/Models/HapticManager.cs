87f348a1dec3353028ab5278f62dd0cf2e647410
austinhteng <austinhteng@gmail.com>
2025-06-30 22:56:21 -0500

    Created haptic manager class

---
+++ HapticLibrary/Models/HapticManager.cs
@@ -0,0 +1,42 @@
+﻿using Datafeel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HapticLibrary.Models
+{
+    /**
+     * Singleton class, stores Datafeel DotManager
+     */
+    public sealed class HapticManager
+    {
+        private static HapticManager _instance;
+        private DotManager _dotManager;
+        public DotManager DotManager { get { return _dotManager; } }
+
+
+        private HapticManager() 
+        {
+            _dotManager = new DotManagerConfiguration().CreateDotManager();
+
+            _dotManager.AddDot(new ManagedDot(_dotManager, 0));
+            _dotManager.AddDot(new ManagedDot(_dotManager, 1));
+            _dotManager.AddDot(new ManagedDot(_dotManager, 2));
+            _dotManager.AddDot(new ManagedDot(_dotManager, 3));
+            _dotManager.AddDot(new ManagedDot(_dotManager, 4));
+
+            _dotManager.Start();
+        }
+
+        public static HapticManager GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new HapticManager();
+            }
+            return _instance;
+        }
+    }
+}
--- HapticLibrary/ViewModels/MainViewModel.cs
+++ HapticLibrary/ViewModels/MainViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Datafeel;
+using HapticLibrary.Models;
 using System.Threading.Tasks;
 
 namespace HapticLibrary.ViewModels;
@@ -16,5 +18,21 @@ public partial class MainViewModel : ViewModelBase
     private async Task AddAudioTextHapticsAsync()
     {
 
+    }
+
+
+    [RelayCommand]
+    private async Task TestHapticsAsync()
+    {
+        HapticManager hapticManager = HapticManager.GetInstance();
+        
+        foreach (var d in hapticManager.DotManager.Dots)
+        {
+            d.LedMode = LedModes.GlobalManual;
+            d.GlobalLed.Red = 255;
+            d.GlobalLed.Green = 0;
+            d.GlobalLed.Blue = 255;
+            d.Write();
+        }
     }
 }
