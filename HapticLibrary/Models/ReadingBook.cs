d95d3aee7e294b933a7d117b352860c388a3533c
austinhteng <austinhteng@gmail.com>
2025-07-06 20:14:26 -0500

    Added code to traverse a reading book

---
+++ HapticLibrary/Models/ReadingBook.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HapticLibrary.Models
+{
+    /**
+     * Stores pages in a haptic reading book.
+     */
+    public class ReadingBook
+    {
+        private ReadingPage[]? pages;
+        private int _pageIndex = 0;
+        public int PageIndex { get { return _pageIndex; } }
+
+        public ReadingBook()
+        {
+            pages = null;
+        }
+
+        public void LoadBook(string bookID) //TODO: ID or name?
+        {
+            //TODO: Get book contents from server
+
+            pages = new ReadingPage[3];
+
+            pages[0] = new ReadingPage("This is the first page", "");
+            pages[1] = new ReadingPage("This is the second page", "");
+            pages[2] = new ReadingPage("This is the last page", "");
+
+            _pageIndex = 0;
+        }
+
+        public string GetText()
+        {
+            return pages[_pageIndex].pageText;
+        }
+
+        public string GetHaptics()
+        {
+            return pages[_pageIndex].pageHaptics;
+        }
+
+        public int GetLength()
+        {
+            return (pages != null) ? pages.Length : 0;
+        }
+
+        public void NextPage()
+        {
+            if (_pageIndex < GetLength() - 1)
+            {
+                _pageIndex++;
+            }
+        }
+
+        public void PreviousPage()
+        {
+            if (_pageIndex > 0)
+            {
+                _pageIndex--;
+            }
+        }
+    }
+
+    /**
+     * Represents a page contents in a haptic reading book.
+     */
+    public class ReadingPage
+    {
+        public string pageText;
+        public string pageHaptics;  //type HapticKeywords
+        public ReadingPage(string PageText, string PageHaptics) 
+        {
+            this.pageText = PageText;
+            this.pageHaptics = PageHaptics;
+        }
+    }
+}
--- HapticLibrary/ViewModels/ReadingPageViewModel.cs
+++ HapticLibrary/ViewModels/ReadingPageViewModel.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using HapticLibrary.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,39 @@ using System.Threading.Tasks;
 
 namespace HapticLibrary.ViewModels
 {
-    public class ReadingPageViewModel : ViewModelBase, IPageViewModel
+    public partial class ReadingPageViewModel : ViewModelBase, IPageViewModel
     {
+        /// <summary>
+        /// Displays current page on main view
+        /// </summary>
+        [ObservableProperty]
+        private string? _bookLine = "BookNotLoaded";
+        [ObservableProperty]
+        private int? _pageNumber = 0;
+
+
+        private ReadingBook _readingBook = new ReadingBook();
+
+        public ReadingPageViewModel()
+        {
+            _readingBook.LoadBook("test");
+            _bookLine = _readingBook.GetText();
+        }
+
+        [RelayCommand]
+        public void NextPage()
+        {
+            _readingBook.NextPage();
+            BookLine = _readingBook.GetText();
+            PageNumber = _readingBook.PageIndex + 1;
+        }
+
+        [RelayCommand]
+        public void PreviousPage()
+        {
+            _readingBook.PreviousPage();
+            BookLine = _readingBook.GetText();
+            PageNumber = _readingBook.PageIndex + 1;
+        }
     }
 }
