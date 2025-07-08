6c64c85b783f8a0b34d52f2a6c885906145c6e09
austinhteng <austinhteng@gmail.com>
2025-07-07 21:10:00 -0500

    Added example reading mode book. Started code for loading file.

--- HapticLibrary/Models/ReadingBook.cs
+++ HapticLibrary/Models/ReadingBook.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Datafeel;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace HapticLibrary.Models
@@ -23,14 +26,38 @@ namespace HapticLibrary.Models
         public void LoadBook(string bookID) //TODO: ID or name?
         {
             //TODO: Get book contents from server
-
+            
             pages = new ReadingPage[3];
 
-            pages[0] = new ReadingPage("This is the first page", "");
-            pages[1] = new ReadingPage("This is the second page", "");
-            pages[2] = new ReadingPage("This is the last page", "");
-
+            pages[0] = new ReadingPage("This is the first page", new Dictionary<string, IDotProps>());
+            pages[1] = new ReadingPage("This is the second page", new Dictionary<string, IDotProps>());
+            pages[2] = new ReadingPage("This is the last page", new Dictionary<string, IDotProps>());
+            DotProps dotProps = new DotProps(new DotPropsJson());
+            TrackPlayer
             _pageIndex = 0;
+
+            string fullPath = Path.Combine(Directory.GetCurrentDirectory(), "Assets", "HapticReadingBookExample.json");
+            string jsonString = File.ReadAllText(fullPath);
+
+            // Parse the JSON into a JsonDocument
+            using JsonDocument doc = JsonDocument.Parse(jsonString);
+            JsonElement root = doc.RootElement;
+
+            // Access data dynamically
+            var user = root.GetProperty("user");
+            string name = user.GetProperty("name").GetString();
+            int id = user.GetProperty("id").GetInt32();
+
+            bool active = root.GetProperty("active").GetBoolean();
+
+            var roles = root.GetProperty("roles");
+            foreach (JsonElement role in roles.EnumerateArray())
+            {
+                Console.WriteLine($"Role: {role.GetString()}");
+            }
+
+            // Print results
+            Console.WriteLine($"Name: {name}, ID: {id}, Active: {active}");
         }
 
         public string GetText()
@@ -38,9 +65,9 @@ namespace HapticLibrary.Models
             return pages[_pageIndex].pageText;
         }
 
-        public string GetHaptics()
+        public Dictionary<string, IDotProps> GetHaptics()
         {
-            return pages[_pageIndex].pageHaptics;
+            return pages[_pageIndex].wordTriggers;
         }
 
         public int GetLength()
@@ -71,11 +98,11 @@ namespace HapticLibrary.Models
     public class ReadingPage
     {
         public string pageText;
-        public string pageHaptics;  //type HapticKeywords
-        public ReadingPage(string PageText, string PageHaptics) 
+        public Dictionary<string, IDotProps> wordTriggers;       //Should this store dotProps or dotPropsJson? Shouldn't matter, Props is probably more correct, json is probably easier maybe??
+        public ReadingPage(string PageText, Dictionary<string, IDotProps> PageHaptics) 
         {
             this.pageText = PageText;
-            this.pageHaptics = PageHaptics;
+            this.wordTriggers = PageHaptics;
         }
     }
 }
